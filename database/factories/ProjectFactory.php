2b0ffbf0063fbe8a233b62e1df38bc94b38584bd
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-03-19 17:33:09 +0100

    creating and using seeder

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,14 @@ class ProjectFactory extends Factory
      */
     public function definition(): array
     {
+
+        $title = fake()->text(20);
+
         return [
-            //
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'content' => fake()->paragraphs(15, true),
+            'image' => fake()->imageUrl(250, 250, true)
         ];
     }
 }
